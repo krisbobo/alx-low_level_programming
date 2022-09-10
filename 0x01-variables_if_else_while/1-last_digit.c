bc3f3adba2472019e1791ee64080c4a1c370e713
krisbobo <kiyongbohbo@gmail.com>
2022-09-09 18:27:25 -0700

    Add code which prints the last digit of the number stored in n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,23 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: print all aplhabet letters
+ * Description: print the value of n status:
+ *              greater than, is zero and is not less than 6.
  *
- * Return: Always 0 (Success)
+ * Return: Always O (Success)
 */
 
 int main(void)
 {
-	char ch = 'a';
+	int n, digit;
 
-	while (ch <= 'z')
-	{
-		putchar(ch);
-		++ch;
-	}
-	putchar('\n');
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/*my code*/
+	digit = n % 10; /*gets last digit*/
+
+	if (digit > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
+	else if (digit == 0)
+		printf("Last digit of %i is %i and is 0\n", n, digit);
+	else if (digit < 6 && digit != 0)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
 
 	return (0);
 }
