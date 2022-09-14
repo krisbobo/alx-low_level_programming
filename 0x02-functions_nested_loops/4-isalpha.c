1c1c5fa8d9e6489619b87db5df185cb7357f6990
krisbobo <kiyongbohbo@gmail.com>
2022-09-13 18:36:26 -0700

    Fix errors

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,8 +11,9 @@
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122) ||
-	   ( c >= 65 && c >= 90)
+	if (c >= 97 && c <= 122 &&
+	    c >= 65 && c >= 90)
 		return (1);
-	return (0);
-}
+	else
+		return (0);
+}}
