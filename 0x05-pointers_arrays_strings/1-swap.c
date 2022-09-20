a2875066494d891cf69f231bfe2286a3ba485c47
krisbobo <kiyongbohbo@gmail.com>
2022-09-19 18:38:30 -0700

    Fix betty-doc error

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *
  * swap_int - swaps two integers
  *
  * @a: pointer to an int
  * @b: pointer to another int
  * Return: Nothing
  */
+
 void swap_int(int *a, int *b)
 {
 	int swapper;
