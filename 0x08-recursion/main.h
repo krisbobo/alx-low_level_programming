f409cc7e88c71436a628183c2eb9fb1c9a3b4767
krisbobo <kiyongbohbo@gmail.com>
2022-09-29 18:18:05 -0700

    Add prototype

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int getNumSqr(int num, int i);
 int is_prime_number(int n);
 
 #endif /* MAIN_H */
