343ebd878d1c25779afc94ac99314503987ac308
krisbobo <kiyongbohbo@gmail.com>
2022-10-04 18:15:27 -0700

    Write a function that concatenates all the arguments of my program

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argument count
+ * @av: argument vector
+ * Return: char
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *ar, *str;
+	int i, j, cont;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			j++;
+			cont++;
+		}
+		cont++;
+	}
+	cont += 1;
+	ar = malloc(cont * sizeof(char));
+	if (ar == NULL)
+		return (NULL);
+	str = ar;
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			*ar = av[i][j];
+			j++;
+			ar++;
+		}
+		*ar = '\n';
+		ar++;
+	}
+	return (str);
+}
