b1a2cf6d8b3f05e60decffae7a60d603bdfa6094
krisbobo <kiyongbohbo@gmail.com>
2022-10-13 17:07:36 -0700

    Add prototype

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,8 @@
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
 /**
  * struct type - Struct data type
  *
