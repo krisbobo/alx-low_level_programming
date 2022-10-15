bb493e24e2a63ac5c5b3f35816a7b745a7d04f03
krisbobo <kiyongbohbo@gmail.com>
2022-10-14 17:12:52 -0700

    Write a program that prints the opcodes of its own main function
    Output format: the opcodes should be printed in hex, lowercase. Each opcode is two char long

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the opcodes of its own main function
+ *
+ * @argc: count of args
+ * @argv: array of args
+ *
+ * Return: int
+ */
+
+int main(int argc, char **argv)
+{
+	int i, bytes;
+	char *addr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	addr = (char *)main;
+	for (i = 0; i < bytes - 1; i++)
+		printf("%02hhx ", addr[i]);
+	printf("%02hhx\n", addr[i]);
+	return (0);
+}
