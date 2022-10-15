66368ada958fdd600b23b7addbb51d3c96a5e596
krisbobo <kiyongbohbo@gmail.com>
2022-10-14 17:03:26 -0700

    Create file containing the function that selects the correct function

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct func to perform the operation
+ * sked by the user
+ * @s: operator passed as arguement to program
+ * Return: a pointer to the function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (*s == *(ops[i]).op)
+		{
+			return (*(ops[i]).f);
+		}
+		i++;
+	}
+	return (NULL);
+}
