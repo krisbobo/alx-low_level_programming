1b6aa61851b077e03f28f35c3f68712f40ac5992
krisbobo <kiyongbohbo@gmail.com>
2022-10-14 17:06:39 -0700

    Create file containing some functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - funtion to add
+ * @a: input 1
+ * @b: input 2
+ * Return: sum a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - funtion to subtract
+ * @a : input 1
+ * @b: input 2
+ * Return: difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - funtion to multiplies
+ * @a: input 1
+ * @b: input 2
+ * Return: product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function to div
+ * @a: input 1
+ * @b: input 2
+ * Return: division of a by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function to mod
+ * @a: input 1
+ * @b: input 2
+ * Return: remainder of the division of a
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
