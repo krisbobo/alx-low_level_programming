12181ebf669b1e54bd8dd85120be144b4f1139e4
krisbobo <kiyongbohbo@gmail.com>
2022-10-20 18:17:56 -0700

    Update file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,14 +8,17 @@
  * Return: number of nodes
  */
 
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
-	size_t count;
+	size_t n_nodes = 0;
+	const listint_t *temp_h;
 
-	for (count = 0; h; count++)
+	temp_h = h;
+	while (temp_h)
 	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+		printf("%d\n", temp_h->n);
+		temp_h = temp_h->next;
+		n_nodes++;
 	}
-	return (count);
+	return (n_nodes);
 }
