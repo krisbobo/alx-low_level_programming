f572d4cffb8065c48bbc7d6fbe094de13a65acc7
krisbobo <kiyongbohbo@gmail.com>
2022-10-20 17:00:50 -0700

    Update file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,22 +10,28 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t cont = 0;
+	size_t count = 0;
 
-	while (h != NULL)
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
 	{
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 			h = h->next;
-			cont++;
+			count++;
 		}
 		else
 		{
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 			h = h->next;
-			cont++;
+			count++;
 		}
 	}
-	return (cont);
+
+	printf("[%d] %s\n", h->len, h->str);
+
+	return (count);
 }
