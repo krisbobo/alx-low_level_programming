2df3a88cda7cfab63dc6744a53d780d9d455d794
krisbobo <kiyongbohbo@gmail.com>
2022-10-20 19:12:30 -0700

    Remove prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,6 +22,5 @@ list_t *add_node(list_t **head, const char *str);
 int _strlen(const char *s);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void *_strdup(const char *src);
 
 #endif /* #ifndef LISTS_H */
