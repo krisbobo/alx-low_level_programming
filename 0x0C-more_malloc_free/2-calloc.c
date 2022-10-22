fcbffb155f09145678bd60ccfb534e31c797830d
krisbobo <kiyongbohbo@gmail.com>
2022-10-21 18:51:15 -0700

    Write a function that allocates memory for an array, using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _calloc - function that allocates memory for an array, using malloc
+ * @nmemb: input size 1
+ * @size: input size bytes
+ * Return: nothing
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *cal;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	cal = malloc(nmemb * size);
+	if (cal == NULL)
+		return (NULL);
+	_memset(cal, 0, nmemb * size);
+	return (cal);
+}
+/**
+ * _memset - function that fills memory with a constant byte.
+ *
+ * @s: mamory area to return
+ * @b: constant byte
+ * @n: size of bytes
+ * Return: char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+
+		*(s + i) = b;
+		i += 1;
+	}
+	return (s);
+}
